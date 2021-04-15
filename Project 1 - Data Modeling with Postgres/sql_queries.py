946b3a248ac3db7b5907399d24b9dc9992a17971
Ashwin Chidambaram <35621386+ashwinchidambaram@users.noreply.github.com>
2021-04-15 00:39:48 -0700

    finished create table logic

--- Project 1 - Data Modeling with Postgres/sql_queries.py
+++ Project 1 - Data Modeling with Postgres/sql_queries.py
@@ -1,50 +1,91 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP table songplays"
-user_table_drop = "DROP table users"
-song_table_drop = "DROP table songs"
-artist_table_drop = "DROP table artists"
-time_table_drop = "DROP table time"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+
+    CREATE TABLE IF NOT EXISTS songplays(
+        songplay_id varchar PRIMARY KEY,
+        start_time timestamp,
+        user_id varchar NOT NULL,
+        level varchar,
+        song_id varchar,
+        artist_id varchar,
+        session_id int,
+        location varchar,
+        user_agent varchar)
 """)
 
 user_table_create = ("""
+
+    CREATE TABLE IF NOT EXISTS users(
+        user_id varchar PRIMARY KEY,
+        first_name varchar,
+        last_name varchar,
+        gender varchar,
+        level varchar)
 """)
 
 song_table_create = ("""
+
+    CREATE TABLE IF NOT EXISTS songs(
+        song_id varchar PRIMARY KEY,
+        title varchar,
+        artist_id varchar,
+        year varchar,
+        duration float)
 """)
 
 artist_table_create = ("""
+
+    CREATE TABLE IF NOT EXISTS artists(
+        artist_id varchar PRIMARY KEY,
+        name varchar,
+        location varchar,
+        latitude float,
+        longitude float)
 """)
 
 time_table_create = ("""
+
+    CREATE TABLE IF NOT EXISTS time(
+        start_time timestamp PRIMARY KEY,
+        hour int,
+        day int,
+        week int,
+        month int,
+        year int,
+        weekday int)
 """)
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
-""")
+#songplay_table_insert = ("""
+#""")
 
-user_table_insert = ("""
-""")
+#user_table_insert = ("""
+#""")
 
-song_table_insert = ("""
-""")
+#song_table_insert = ("""
+#""")
 
-artist_table_insert = ("""
-""")
+#artist_table_insert = ("""
+#""")
 
 
-time_table_insert = ("""
-""")
+#time_table_insert = ("""
+#""")
 
 # FIND SONGS
 
-song_select = ("""
-""")
+#song_select = ("""
+#""")
 
 # QUERY LISTS
 
