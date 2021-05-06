3a7b27b458570ca9e3f1bf0ffc736fa723936e3a
Ashwin Chidambaram <35621386+ashwinchidambaram@users.noreply.github.com>
2021-05-05 19:32:06 -0700

    Finishing touches on Instructor comments

--- Project 1 - Data Modeling with Postgres/sql_queries.py
+++ Project 1 - Data Modeling with Postgres/sql_queries.py
@@ -68,7 +68,6 @@ time_table_create = ("""
 songplay_table_insert = ("""
 
     INSERT INTO songplays(
-        songplay_id,
         start_time,
         user_id,
         level,
@@ -78,7 +77,7 @@ songplay_table_insert = ("""
         location,
         user_agent)
 
-    VALUES(DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s)
+    VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
     ON CONFLICT DO NOTHING
 
 """)
@@ -93,7 +92,7 @@ user_table_insert = ("""
         level)
 
     VALUES(%s, %s, %s, %s, %s)
-    ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level
+    ON CONFLICT (user_id) DO UPDATE SET level = excluded.level
 
 """)
 
